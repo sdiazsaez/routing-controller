7c4531df78d96af3b5c960ab670b9a167be67b3a
simon <sdiaz.sz@gmail.com>
2020-12-16 17:52:03 -0300

    [Update] Laravel 7 change namespace path for json resource

--- src/Resource.php
+++ src/Resource.php
@@ -1,30 +1,21 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: simon
- * Date: 3/19/18
- * Time: 14:21
- */
 
 namespace Larangular\RoutingController;
 
-use Illuminate\Http\Resources\Json\Resource as BaseResource;
+use Illuminate\Http\Resources\Json\JsonResource as BaseResource;
 
-class Resource extends BaseResource
-{
+class Resource extends BaseResource {
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return array
      */
-    public function toArray($request)
-    {
+    public function toArray($request) {
         return parent::toArray($request);
     }
 
-    public function withResponse($request, $response)
-    {
+    public function withResponse($request, $response) {
         $response->header('Content-Type', 'application/vnd.api+json');
     }
 }
