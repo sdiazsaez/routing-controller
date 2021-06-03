297b3b870fde2511381350d01714a35ba5b1ecf4
Simon <sdiaz.sz@gmail.com>
2021-06-03 01:01:08 -0400

    support binding routes

--- src/RoutingRequests.php
+++ src/RoutingRequests.php
@@ -2,67 +2,100 @@
 
 namespace Larangular\RoutingController;
 
+use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
+use Larangular\Support\Facades\Instance;
 
-trait RoutingRequests {
+trait RoutingRequests
+{
 
-    abstract public function allowedMethods();
-
-    abstract public function entries($where = []);
-
-    abstract public function entry($id);
-
-    abstract public function save($data);
-
-    abstract public function delete($id);
-
-    abstract public function getUpdateMethod(array $params): string;
-
-    public function index(Request $request) {
+    public function index(Request $request)
+    {
         $this->prepareRequest(__FUNCTION__);
         return $this->entries($request->all());
     }
 
-    public function show($id) {
+    protected function prepareRequest($method): void
+    {
+        if (!$this->isRequestAllowed($method)) {
+            $this->abortRequest();
+        }
+    }
+
+    private function isRequestAllowed($method): bool
+    {
+        return \in_array($method, $this->allowedMethods());
+    }
+
+    abstract public function allowedMethods();
+
+    private function abortRequest(): void
+    {
+        abort(403, 'Unauthorized');
+    }
+
+    abstract public function entries($where = []);
+
+    public function show($id)
+    {
         $this->prepareRequest(__FUNCTION__);
         return $this->entry($id);
     }
 
-    public function create() { }
+    abstract public function entry($id);
 
-    public function store(Request $request) {
+    public function create()
+    {
+    }
+
+    public function store(Request $request)
+    {
         $this->prepareRequest(__FUNCTION__);
         return $this->save($request->all());
     }
 
-    public function edit($id) { }
+    abstract public function save($data);
 
-    public function update(Request $request, $id) {
+    public function edit($id)
+    {
+    }
+
+    public function update(Request $request, $id)
+    {
         $this->prepareRequest(__FUNCTION__);
 
         $data = $request->all();
-        $data['id'] = $id;
+        $data['id'] = $this->getBindId($id);
 
         $method = $this->getUpdateMethod($data);
         return $this->{$method}($data);
     }
 
-    public function destroy($id) {
+    private function getBindId($unknown)
+    {
+        if (Instance::instanceOf($unknown, Collection::class)) {
+            $unknown = $unknown->first();
+        }
+
+        if (Instance::instanceOf($unknown, Arrayable::class)) {
+            $unknown = $unknown->toArray();
+        }
+
+        if (!\is_numeric($unknown) && array_key_exists('id', $unknown)) {
+            $unknown = $unknown['id'];
+        }
+
+        return $unknown;
+    }
+
+    abstract public function getUpdateMethod(array $params): string;
+
+    public function destroy($id)
+    {
         $this->prepareRequest(__FUNCTION__);
         return $this->delete($id);
     }
 
-    protected function prepareRequest($method) {
-        if(!$this->isRequestAllowed($method)) {
-            $this->abortRequest();
-        }
-    }
-
-    private function isRequestAllowed($method) {
-        return \in_array($method, $this->allowedMethods());
-    }
-
-    private function abortRequest() {
-        abort(403, 'Unauthorized');
-    }
+    abstract public function delete($id);
 }
